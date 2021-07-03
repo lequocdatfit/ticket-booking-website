95c9442458a9008271e88e61ea39ecc89f114e2c
HaruKishima <18120256@student.hcmus.edu.vn>
2021-07-03 21:06:45 +0700

    fix booking with ticket

--- server/controller/booking.controller.js
+++ server/controller/booking.controller.js
@@ -1,44 +1,57 @@
-const bookingModel = require('../model/bookings.model');
-const ticketModel = require('../model/tickets.model');
+const bookingModel = require("../model/bookings.model");
+const ticketModel = require("../model/tickets.model");
 
 module.exports.addBooking = (req, res) => {
-  bookingModel.create(req.body).then((result) => {
-    res.status(201).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .create(req.body)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
 
 module.exports.findById = (req, res) => {
-  bookingModel.findById(req.params.id).then((result) => {
-    res.status(200).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .findById(req.params.id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
 
 module.exports.findByPNR = (req, res) => {
-  bookingModel.find(req.params.pnr).then((result) => {
-    res.status(200).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .find(req.params.pnr)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
 
 module.exports.patchBooking = (req, res) => {
-  bookingModel.update(req.params.id, req.body).then((result) => {
-    res.status(200).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .update(req.params.id, req.body)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
 
 module.exports.list = (req, res) => {
-  let limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+  let limit =
+    req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
   let page = 0;
   if (req.query) {
     if (req.query.page) {
@@ -46,34 +59,41 @@ module.exports.list = (req, res) => {
       page = Number.isInteger(req.query.page) ? req.query.page : 0;
     }
   }
-  bookingModel.list(limit, page).then((result) => {
-    res.status(200).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .list(limit, page)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
 
 module.exports.deleteBooking = (req, res) => {
-  bookingModel.delete(req.params.id).then((result) => {
-    res.status(200).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .delete(req.params.id)
+    .then((result) => {
+      res.status(200).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
 
 module.exports.createBookingAndTickets = async (req, res) => {
   let ticketInfos;
-  if (req.body.hasOwnProperty('ticketInfos')) {
+  if (req.body.hasOwnProperty("ticketInfos")) {
     ticketInfos = JSON.parse(req.body.ticketInfos);
   } else if (!Array.isArray(ticketInfos)) {
-    return res.status(400).json({ errors: ['Does not found any tickets'] });
+    return res.status(400).json({ errors: ["Does not found any tickets"] });
   }
   let ticketObjects = [];
+  console.log(ticketObjects);
   try {
-    let ticketPromises = ticketInfos.map(element => {
-      return ticketModel.create(element._id);
+    let ticketPromises = ticketInfos.map((element) => {
+      return ticketModel.create(element);
     });
     ticketObjects = await Promise.all(ticketPromises);
   } catch (e) {
@@ -82,14 +102,17 @@ module.exports.createBookingAndTickets = async (req, res) => {
   }
   req.body.tickets = [];
   req.body.totalPrice = 0;
-  ticketObjects.forEach(element => {
+  ticketObjects.forEach((element) => {
     req.body.tickets.push(element._id);
     req.body.totalPrice += element.price;
   });
-  bookingModel.create(req.body).then((result) => {
-    res.status(201).json(result);
-  }).catch((e) => {
-    console.error(e);
-    res.status(500).json({ errors: e });
-  });
-}
+  bookingModel
+    .create(req.body)
+    .then((result) => {
+      res.status(201).json(result);
+    })
+    .catch((e) => {
+      console.error(e);
+      res.status(500).json({ errors: e });
+    });
+};
