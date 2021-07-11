2bf4369d35a769a827f50e41b03babb602db29fd
HaruKishima <18120256@student.hcmus.edu.vn>
2021-07-11 19:47:22 +0700

    disable cors

--- server/index.js
+++ server/index.js
@@ -1,47 +1,47 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const key = require('./config/main.config');
-const authenticationRouter = require('./route/authentication.route');
-const userRouter = require('./route/user.route');
-const airportRouter = require('./route/airport.route');
-const airlinerRouter = require('./route/airliner.route');
-const flightRouter = require('./route/flight.route');
-const ticketRouter = require('./route/ticket.route');
-const bookingRouter = require('./route/booking.route');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const key = require("./config/main.config");
+const authenticationRouter = require("./route/authentication.route");
+const userRouter = require("./route/user.route");
+const airportRouter = require("./route/airport.route");
+const airlinerRouter = require("./route/airliner.route");
+const flightRouter = require("./route/flight.route");
+const ticketRouter = require("./route/ticket.route");
+const bookingRouter = require("./route/booking.route");
 
 const { port, mongoUrl, corsOrigin } = key;
 
 const app = express();
 
-require('./db').connectMongoDb(mongoUrl);
+require("./db").connectMongoDb(mongoUrl);
 
 var corsOption = {
   origin: corsOrigin,
-  optionsSuccessStatus: 200
-}
+  optionsSuccessStatus: 200,
+};
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(cors(corsOption));
+//app.use(cors(corsOption));
 
-app.get('/', (req, res) => {
+app.get("/", (req, res) => {
   res.json({ success: true });
 });
 
-app.use('/authentication', authenticationRouter);
+app.use("/authentication", authenticationRouter);
 
-app.use('/user', userRouter);
+app.use("/user", userRouter);
 
-app.use('/airport', airportRouter);
+app.use("/airport", airportRouter);
 
-app.use('/airliner', airlinerRouter);
+app.use("/airliner", airlinerRouter);
 
-app.use('/flight', flightRouter);
+app.use("/flight", flightRouter);
 
-app.use('/ticket', ticketRouter);
+app.use("/ticket", ticketRouter);
 
-app.use('/booking', bookingRouter);
+app.use("/booking", bookingRouter);
 
 app.use(function (req, res) {
   res.status(404).json({ errors: ["Not found"] });
@@ -54,4 +54,4 @@ app.use(function (err, req, res, next) {
 
 app.listen(port, () => {
   console.log("Server is running at " + port);
-})
+});
